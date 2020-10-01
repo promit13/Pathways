9fd93baab02fd3514ba427269a9815bb17423841
promit13 <rai.promit@gmail.com>
2020-10-01 14:11:12 +0100

    Update firebase.js

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -2,19 +2,16 @@ import * as firebase from "firebase";
 import "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB5Xph1yPLyuZcoeAnvgzTtoTO4A2Jpg3Y",
-  authDomain: "pathways-429eb.firebaseapp.com",
-  databaseURL: "https://pathways-429eb.firebaseio.com",
-  projectId: "pathways-429eb",
-  storageBucket: "pathways-429eb.appspot.com",
-  messagingSenderId: "787179059970",
-  appId: "1:787179059970:web:cd0ad3a48eced7089ce01b",
-  measurementId: "G-2K1SZRR87N"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
-
-// export const geo = geofirex.init(firebase);
-// export const GOOGLE_API = "AIzaSyAzny6yRXLVY91uAUPZAKRtwpU3pMcP7VI";
 
 export default firebase;
